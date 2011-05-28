0d72ae5e279f44d3aea70e4792bf244f406be8b1
Alex Skorulis <skorulis@gmail.com>
2011-05-28 21:31:02 +1000

    Initial commit

---
+++ src/com/skorulis/heli2/ui/StateImage.java
@@ -0,0 +1,55 @@
+package com.skorulis.heli2.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import forplay.core.Image;
+
+/**
+ * Represents a list of states that map to Images. A state is considered to match if all the true flags in the combo are on in the supplied flag
+ * @author Alex
+ */
+public class StateImage {
+
+	private List<StateImageCombo> combos;
+	
+	/**
+	 * Create a state with a single combo
+	 * @param state state to match
+	 * @param image image to return
+	 */
+	public StateImage(int state,Image image) {
+		this(new int[]{state},new Image[]{image});
+	}
+	
+	public StateImage(int[] states,Image[] images) {
+		if(states.length!=images.length) {
+			throw new IllegalArgumentException("number of states and images must match");
+		}
+		combos = new ArrayList<StateImageCombo>();
+		for(int i=0;i < states.length; ++i) {
+			combos.add(new StateImageCombo(states[i],images[i]));
+		}
+	}
+	
+	public Image getImage(int state) {
+		for(StateImageCombo combo: combos) {
+			if( (combo.state & state) == combo.state) {
+				return combo.image;
+			}
+		}
+		return null;
+	}
+	
+	private static class StateImageCombo {
+		
+		public StateImageCombo(int i, Image image) {
+			this.state = i;
+			this.image = image;
+		}
+		private int state;
+		private Image image;
+		
+	}
+	
+}
